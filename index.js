aa274d812be2cd730a3e499be304c6c090b3f44d
Bradley Farias <bfarias@godaddy.com>
2016-04-28 10:08:16 -0700

    move from `stop` to `guess`

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var formatRegExp = /%[sdj%]/g;
 
-exports.indexOfMeta = function (args, stop) {
+exports.indexOfMeta = function (args, guess) {
   if (args.length < 2) {
     return -1;
   }
-  stop = typeof stop !== 'undefined' ? stop : args.length;
-  var meta_index = stop - 1;
+  var meta_index = typeof guess !== 'undefined' ? guess : args.length - 1;
   var metaType  = Object.prototype.toString.call(args[meta_index]);
   var fmtMatch  = args[0] && args[0].match && args[0].match(formatRegExp);
   var isFormat  = fmtMatch && fmtMatch.length;
